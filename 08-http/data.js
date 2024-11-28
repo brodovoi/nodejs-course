a0f5502cb52b1e4ca06038e46b775a1ec2d22eba
Artem Borodovoy <a.borodovoi@gmail.com>
2024-11-28 02:20:16 +0200

    module http

---
+++ 08-http/data.js
@@ -0,0 +1,7 @@
+const comments = [
+  { id: 100, text: 'First comment', author: 'Artem' },
+  { id: 526, text: 'Second comment', author: 'Alice' },
+  { id: 724, text: 'Last comment', author: 'Bobe' },
+];
+
+module.exports = comments;
